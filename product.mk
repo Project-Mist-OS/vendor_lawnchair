7e073f7f76d0db745800c5e3836c87b052ee65f2
janak <niraulajanak2019@gmail.com>
2024-07-04 22:39:36 +0545

    extra: adapt for Everest

--- product.mk
+++ product.mk
@@ -1,10 +1,4 @@
-# TARGET_LAUNCHER options:
-# 1 - stock (default)
-# 2 - lawnchair
-# 3 - pixel (valid only on gapps builds)
-ifeq ($(TARGET_LAUNCHER),2)
   PRODUCT_PACKAGES += \
     Lawnchair \
     LawnchairOverlay \
     Lawnicons
-endif
